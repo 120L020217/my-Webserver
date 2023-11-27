1c5fc81a082d88fadb8f2ca86322264e39a8ce6f
coldcodacode <gzs_0217@qq.com>
2023-11-27 14:14:37 +0800

    11.27 update

---
+++ myWebServer/process/FIFO/fifo.cpp
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int ret = mkfifo("fifo1", 0664);
+
+    if (ret == -1) {
+        perror("mkfifo");
+        exit(0);
+    }
+
+    
+}
